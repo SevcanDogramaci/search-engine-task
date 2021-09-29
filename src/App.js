8f6125158e035b63cb573c90c8cc7016e2248b44
SevcanDogramaci <sevcan.dog@gmail.com>
2021-09-30 00:00:10 +0300

    Add routing

--- src/App.js
+++ src/App.js
@@ -1,25 +1,14 @@
-import logo from './logo.svg';
-import './App.css';
+import { BrowserRouter, Route } from 'react-router-dom';
+import LandingPage from './pages/landing/LandingPage';
+import ResultsPage from './pages/results/ResultsPage';
 
 function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+	return (
+		<BrowserRouter>
+			<Route path="/" exact component={LandingPage} />
+			<Route path="/results" component={ResultsPage} />
+		</BrowserRouter>
+	);
 }
 
 export default App;
